0649ba2ea054d73f10454ce1a2d1988cb03e8aea
Giuseppe Basile <giuseppe@novoda.com>
2019-09-03 14:33:25 +0200

    GET /movie with simple JSON

--- Package.swift
+++ Package.swift
@@ -6,13 +6,14 @@ import PackageDescription
 let package = Package(
     name: "swift-server",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.7.0")
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.7.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "swift-server",
-            dependencies: ["Kitura"])
+            dependencies: ["Kitura", "SwiftyJSON"])
     ]
 )
--- Sources/swift-server/main.swift
+++ Sources/swift-server/main.swift
@@ -1,10 +1,18 @@
 import Kitura
+import SwiftyJSON
 
 let router = Router()
 
 router.get("/") { request, response, next in
     response.send("Hello world!")
     next()
+}
+
+router.get("/movie") { request, response, next in
+	let movie = JSON(["id":  1, "title": "Pulp Fiction"])
+	response.send(json: movie)
+
+	next()
 }
 
 Kitura.addHTTPServer(onPort: 8080, with: router)
