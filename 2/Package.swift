49d2ddab3b4716b01b0d12915413e80fc358a916
Giuseppe Basile <giuseppe@novoda.com>
2019-09-05 15:52:39 +0200

    build project with new version of swift and dependencies

--- 2/Package.swift
+++ 2/Package.swift
@@ -1,16 +1,24 @@
-// swift-tools-version:3.1
+// swift-tools-version:5.0.0
 import PackageDescription
 
 let package = Package(
     name: "monolith",
-    targets: [
-        Target(name: "monolith")
-    ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsHTTP.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 1)
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.8.0"),
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.9.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", from: "2.4.1"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-CredentialsHTTP.git", from: "2.1.3"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "3.2.0")
+    ],
+    targets: [
+        .target(
+            name: "monolith",
+            dependencies: [
+                "Kitura", 
+                "HeliumLogger", 
+                "Credentials", 
+                "CredentialsHTTP", 
+                "PerfectCrypto"
+            ])
     ]
 )
